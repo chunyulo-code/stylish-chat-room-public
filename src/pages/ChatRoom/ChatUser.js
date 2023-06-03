6f89be2df55d6725c57153abf75a5e1557fa240c
Vito Huang <vito.formosa@outlook.com>
2023-06-03 22:30:25 +0800

    chore: Add pull request template

--- src/pages/ChatRoom/ChatUser.js
+++ src/pages/ChatRoom/ChatUser.js
@@ -1,25 +1,3 @@
-import Split from "react-split";
-
 export default function ChatUser() {
-  return (
-    <>
-      <Split
-        sizes={[25, 75]}
-        minSize={100}
-        expandToMin={false}
-        gutterSize={10}
-        gutterAlign="center"
-        snapOffset={30}
-        dragInterval={1}
-        direction="horizontal"
-        cursor="col-resize"
-        className="flex"
-      >
-        <h1 className="text-3xl font-bold underline bg-sky-400">Hello Admin</h1>
-        <h2 className="text-2xl font-bold underline bg-violet-400">
-          Hello Admin
-        </h2>
-      </Split>
-    </>
-  );
+  return <></>;
 }
