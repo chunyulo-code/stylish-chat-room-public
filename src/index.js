95cb844dd1bfe1405e64e973bfeff520203a5f82
Vito Huang <vito.formosa@outlook.com>
2023-06-04 21:08:03 +0800

    style(chat-user): Complete layout of chat-user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./index.css";
 
 import App from "./App";
 import Checkout from "./pages/Checkout/Checkout";
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xl: { max: "1280px" },
+      // => @media (max-width: 1280px) { ... }
+
+      lg: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      md: { max: "480px" },
+      // => @media (max-width: 767px) { ... }
+    },
   },
   plugins: [],
 };
